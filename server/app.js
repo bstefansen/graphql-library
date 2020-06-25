92ff28f731b99cb4dcc06dd08b545d6b675d2970
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-06-25 15:43:18 -0400

    first commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connect to mongoDB database
+// mongoDB won't connect
+mongoose.connect('mongodb+srv://bstefansen:testing1234@cluster0-mbz6o.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+})

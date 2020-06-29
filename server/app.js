4f3a8e1d7423d87b0489d459b18a7c995bc594ea
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-06-29 13:22:14 -0400

    added .env

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 // connect to mongoDB database
 // mongoDB won't connect
-mongoose.connect('mongodb+srv://bstefansen:testing1234@cluster0-mbz6o.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('connected to database');
 })
